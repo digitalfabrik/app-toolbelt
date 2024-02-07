e23d88abe1b1229544e4ab169c47317c98293510
ztefanie <stefanie.metzger@tuerantuer.org>
2024-02-07 08:29:38 +0100

    Convert notify script to ts

--- src/notify/notify.ts
+++ src/notify/notify.ts
@@ -1,23 +1,49 @@
 import { exec } from 'child_process'
 import { Command } from 'commander'
+import * as process from 'process'
+import fetch from 'node-fetch'
 
 export default (parent: Command) => {
   parent
     .command('mattermost')
-    .requiredOption('--message <message>', 'The message that should be sent')
-    .requiredOption('--channel <channel>', '')
+    .option('--message <message>', 'The message that should be sent')
+    .option('--channel <channel>', '')
     .option('--allow-all-branches', '')
     .description('Send a message to a mattermost channel')
-    .action((options: { [key: string]: any }) => {
-      exec(
-        `./src/notify/notify-mattermost ${options.channel} ${options.message} ${
-          options.allowAllBranches ? '--allow-all-branches' : ''
-        }`,
-        (error, stdout, stderr) => {
-          if (error) console.log('Error: ', error?.message)
-          if (stderr) console.log('Stderr: ', stderr)
-          console.log(stdout)
-        }
-      )
+    .action(async (options: { [key: string]: any }) => {
+      if (process.env.CIRCLE_BRANCH != 'main' && !options.allowAllBranches) {
+        console.log('Not on main branch. Skipping.')
+        process.exit(0)
+      }
+
+      if (!options.message) {
+        console.log('No message set. Skipping.')
+      }
+
+      if (!process.env.MM_WEBHOOK) {
+        console.log('NO MATTERMOST WEBHOOK SET')
+        console.log('Please add the environment variable "MM_WEBHOOK" in the settings for this project.')
+        process.exit(1)
+      }
+
+      const data = {
+        channel: options.channel,
+        username: 'circleci',
+        text: options.message
+      }
+
+      const response = await fetch(process.env.MM_WEBHOOK, {
+        method: 'post',
+        body: JSON.stringify(data),
+        headers: { 'Content-Type': 'application/json' }
+      })
+
+      if (response.status != 200) {
+        console.error('Notification not sent due to an error. Status: $STATUS. Please check the webhook URL')
+        process.exit(1)
+      }
+
+      console.log('Notification sent!')
+      process.exit(0)
     })
 }
