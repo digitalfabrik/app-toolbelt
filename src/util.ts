c18ebc44e3b8bb7e465c4710b4a3f2774c09d8ea
Steffen Kleinle <steffen.kleinle@mailbox.org>
2025-05-14 19:07:50 +0200

    Fix versioning for integreat by searching for version.json in parents

--- src/util.ts
+++ src/util.ts
@@ -1,4 +1,6 @@
 import { Platform, PLATFORMS } from './constants.js'
+import path from 'path'
+import fs from 'fs'
 
 export const nonNullablePredicate = <T>(value: T): value is NonNullable<T> => value !== null && value !== undefined
 
@@ -16,4 +18,18 @@ export const getPlatformsFromString = (platformsString?: string): Platform[] | u
     )
   }
   return platforms
+}
+
+export const findPathInParents= (name: string, directory: string = '.'): string => {
+  let currentDirectory = process.cwd()
+
+  for (let i = 0; i < 8; i++) {
+    const currentPath = path.resolve(currentDirectory, directory, name)
+    if (fs.existsSync(currentPath)) {
+      return currentPath
+    }
+    currentDirectory = path.resolve(currentDirectory, '..')
+  }
+
+  throw new Error(`${name} not found in ${path.resolve(process.cwd(), directory)} or parent directories!`)
 }
