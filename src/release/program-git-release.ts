4f986942525c2427d59233338f2faadc7a32b93f
Andy <fischer@integreat-app.de>
2026-04-16 13:44:48 +0200

    56: add hotfix option, add error handling, create an additional commit for hotfix

--- src/release/program-git-release.ts
+++ src/release/program-git-release.ts
@@ -7,9 +7,12 @@ import {
   withGithubAuthentication,
 } from '../github.js'
 
+const MAIN_BRANCH = 'main'
+
 type GithubBumpVersionOptions = GithubAuthenticationParams & {
   branch: string
   tagOnly: boolean
+  hotfix: boolean
 }
 
 export default (parent: Command) => {
@@ -18,14 +21,19 @@ export default (parent: Command) => {
     .command('bump-to <new-version-name> <new-version-code>')
     .requiredOption('--branch <branch>', 'the current branch')
     .option('--tag-only', 'Only tag the latest commit instead of bumping the version.', false)
+    .option('--hotfix', 'Also bump the version on the main branch.', false)
     .action(async (newVersionName, newVersionCode, options: GithubBumpVersionOptions) => {
       try {
-        const { tagOnly, owner, repo, branch } = options
+        const { tagOnly, hotfix, owner, repo, branch } = options
         const appOctokit = await authenticate(options)
 
         const versionCode = parseInt(newVersionCode, 10)
         if (Number.isNaN(versionCode)) {
           throw new Error(`Failed to parse version code string: ${newVersionCode}`)
+        }
+
+        if (hotfix && !tagOnly && branch === MAIN_BRANCH) {
+          throw new Error('--hotfix cannot be used on the main branch.')
         }
 
         const commitSha = tagOnly
@@ -37,6 +45,10 @@ export default (parent: Command) => {
         }
         const tagMessage = `${newVersionName} (${versionCode})`
         await createTag(newVersionName, tagMessage, owner, repo, commitSha, appOctokit)
+
+        if (hotfix && !tagOnly) {
+          await commitVersion(newVersionName, versionCode, owner, repo, MAIN_BRANCH, appOctokit)
+        }
       } catch (e) {
         console.error(e)
         process.exit(1)
