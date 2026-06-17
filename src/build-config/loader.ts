989a8bb22154569160fe568ba6e5af7a737a120a
André Wachter <andre.w8er@gmail.com>
2026-06-17 14:05:15 +0200

    72: Also try `build-configs/src/<name>` when searching

--- src/build-config/loader.ts
+++ src/build-config/loader.ts
@@ -24,10 +24,15 @@ export const loadBuildConfig = async (
     throw Error(`Invalid platform supplied: ${platform}`)
   }
 
-  const buildConfigPath = findPathInParents(buildConfigDirectory, buildConfigName)
+  const buildConfigPath =
+    findPathInParents(buildConfigDirectory, 'src', buildConfigName) ??
+    findPathInParents(buildConfigDirectory, buildConfigName)
 
   if (!buildConfigPath) {
-    throw new Error(`${join(buildConfigDirectory, buildConfigName)} not found in ${process.cwd()} or parent directories!`)
+    throw new Error(
+      `No '${join(buildConfigDirectory, 'src', buildConfigName)}' or ` +
+        `'${join(buildConfigDirectory, buildConfigName)}' found in ${process.cwd()} or parent directories!`,
+    )
   }
 
   const buildConfig = (await import(buildConfigPath)).default
