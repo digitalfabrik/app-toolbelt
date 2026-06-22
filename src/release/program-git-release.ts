6afd42515c389578222eedaab9e3ee1c09e02efc
Andy <fischer@integreat-app.de>
2026-06-22 16:38:30 +0200

    74: make versionCode optional in program-git-release.ts

--- src/release/program-git-release.ts
+++ src/release/program-git-release.ts
@@ -14,35 +14,57 @@ type GithubBumpVersionOptions = GithubAuthenticationParams & {
   hotfix: boolean
 }
 
+const getCommitSha = async (
+  tagOnly: boolean,
+  newVersionName: string,
+  newVersionCode: string | undefined,
+  owner: string,
+  repo: string,
+  branch: string,
+  appOctokit: Awaited<ReturnType<typeof authenticate>>,
+): Promise<{ commitSha: string; versionCode: number | undefined }> => {
+  const versionCode = newVersionCode !== undefined ? parseInt(newVersionCode, 10) : undefined
+  if (Number.isNaN(versionCode)) {
+    throw new Error(`Failed to parse version code string: ${newVersionCode}`)
+  }
+
+  if (tagOnly) {
+    const commitSha = (await appOctokit.repos.getBranch({ owner, repo, branch })).data.commit.sha
+    return { commitSha, versionCode }
+  }
+  const commitSha = await commitVersion(newVersionName, versionCode, owner, repo, branch, appOctokit)
+  if (!commitSha) {
+    throw new Error('Failed to commit!')
+  }
+  return { commitSha, versionCode }
+}
+
 export default (parent: Command) => {
   const command = parent
-    .command('bump-to <new-version-name> <new-version-code>')
+    .command('bump-to <new-version-name> [new-version-code]')
     .description('Bump and tag the latest version')
     .requiredOption('--branch <branch>', 'the current branch')
     .option('--tag-only', 'Only tag the latest commit instead of bumping the version.', false)
     .option('--hotfix', 'Also bump the version on the main branch.', false)
-    .action(async (newVersionName, newVersionCode, options: GithubBumpVersionOptions) => {
+    .action(async (newVersionName, newVersionCode: string | undefined, options: GithubBumpVersionOptions) => {
       try {
         const { tagOnly, hotfix, owner, repo, branch } = options
         const appOctokit = await authenticate(options)
-
-        const versionCode = parseInt(newVersionCode, 10)
-        if (Number.isNaN(versionCode)) {
-          throw new Error(`Failed to parse version code string: ${newVersionCode}`)
-        }
 
         if (hotfix && (tagOnly || branch === MAIN_BRANCH)) {
           throw new Error('--hotfix cannot be used on the main branch or together with the --tag-only option.')
         }
 
-        const commitSha = tagOnly
-          ? (await appOctokit.repos.getBranch({ owner, repo, branch })).data.commit.sha
-          : await commitVersion(newVersionName, versionCode, owner, repo, branch, appOctokit)
-
-        if (!commitSha) {
-          throw new Error(`Failed to commit!`)
-        }
-        const tagMessage = `${newVersionName} (${versionCode})`
+        const { commitSha, versionCode } = await getCommitSha(
+          tagOnly,
+          newVersionName,
+          newVersionCode,
+          owner,
+          repo,
+          branch,
+          appOctokit,
+        )
+        const tagMessage = versionCode !== undefined ? `${newVersionName} (${versionCode})` : newVersionName
         await createTag(newVersionName, tagMessage, owner, repo, commitSha, appOctokit)
 
         if (hotfix) {
